387a95aef1fcd8f36ce7fd0b47e9ce493af0b53e
Alexis Abril <dev@alexisabril.com>
2011-10-14 18:12:01 -0500

    Production updates for getjs.

--- todo/scripts/build.js
+++ todo/scripts/build.js
@@ -2,5 +2,8 @@
 
 load('steal/rhino/rhino.js');
 steal('steal/build','steal/build/scripts','steal/build/styles',function(){
-	steal.build('todo/scripts/build.html',{to: 'todo'});
+	steal.build('todo/scripts/build.html', {
+		to: 'todo',
+		compressor: 'uglify'
+	});
 });
