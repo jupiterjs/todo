29521abfa0f95e19962d1fdee941d73cb9a20608
David Luecke <daff@neyeon.com>
2012-05-02 14:59:58 -0600

    Initial port to TodoMVC and DoneJS

---
+++ todo/model.js
@@ -0,0 +1,97 @@
+steal('can/model').then(function() {
+
+	// Basic Todo entry model
+	// { text: 'todo', complete: false }
+	can.Model('Todo', {
+
+		// Implement local storage handling
+		localStore: function(cb){
+			var name = 'todos-canjs-jquery',
+				data = JSON.parse( window.localStorage[name] || (window.localStorage[name] = '[]') ),
+				res = cb.call(this, data);
+			if(res !== false){
+				can.each(data, function(i, todo) {
+					delete todo.editing;
+				});
+				window.localStorage[name] = JSON.stringify(data);
+			}
+		},
+
+		findAll: function(params){
+			var def = new can.Deferred();
+			this.localStore(function(todos){
+				var instances = [],
+					self = this;
+				can.each(todos, function(i, todo) {
+					instances.push(new self(todo));
+				});
+				def.resolve({data: instances});
+			})
+			return def;
+		},
+
+		destroy: function(id){
+			var def = new can.Deferred();
+			this.localStore(function(todos){
+				for (var i = 0; i < todos.length; i++) {
+					if (todos[i].id === id) {
+						todos.splice(i, 1);
+						break;
+					}
+				}
+				def.resolve({});
+			});
+			return def
+		},
+
+		create: function(attrs){
+			var def = new can.Deferred();
+			this.localStore(function(todos){
+				attrs.id = attrs.id || parseInt(100000 *Math.random());
+				todos.push(attrs);
+			});
+			def.resolve({id : attrs.id});
+			return def
+		},
+
+		update: function(id, attrs){
+			var def = new can.Deferred();
+			this.localStore(function(todos){
+				for (var i = 0; i < todos.length; i++) {
+					if (todos[i].id === id) {
+						var todo = todos[i];
+						break;
+					}
+				}
+				can.extend(todo, attrs);
+			});
+			def.resolve({});
+			return def
+		}
+
+	},{});
+
+	// List for Todos
+	can.Model.List('Todo.List', {
+
+		completed: function() {
+			// Ensure this triggers on length change
+			this.attr('length');
+
+			var completed = 0;
+			this.each(function(todo, i) {
+				completed += todo.attr('complete') ? 1 : 0
+			});
+			return completed;
+		},
+
+		remaining: function() {
+			return this.attr('length') - this.completed();
+		},
+
+		allComplete: function() {
+			return this.attr('length') === this.completed();
+		}
+	});
+
+});
--- todo/todo.js
+++ todo/todo.js
@@ -1,248 +1,99 @@
-// Load what we need
-steal('jquery/model/list',
-      'jquery/controller',
-      'jquery/view/ejs',
-      'jquery/lang/json',
-      './todo.css',
-      function($){
+steal('can/control', 'can/view/ejs')
+	.then('./model.js',
+		'./styles/base.css',
+		'./styles/todo.css')
+	.then(function() {
 
-/**
- * A todo model for CRUDing todos.
- */
-$.Model('Todo',{
-	/**
-	 * Gets JSON data from localStorage.  Any changes that 
-	 * get made in cb get written back to localStorage.
-	 * 
-	 * This is unimportant for understanding JavaScriptMVC!
-	 */
-	localStore: function(cb){
-		var name = this.shortName,
-			data = $.evalJSON( window.localStorage[name] || (window.localStorage[name] = "{}") ),
-			res = cb.call(this, data);
-		if(res !== false){
-			window.localStorage[name] = $.toJSON(data);
-		}
-	},
-	/**
-	 * Gets todos from localStorage.
-	 * 
-	 *     Todo.findAll({}, success(todos))
-	 */
-	findAll: function(params , success){
-		this.localStore(function(todos){
-			var instances = [];
-			for(var id in todos){
-				instances.push( new this( todos[id]) )
+	can.Control('Todos', {
+
+		// Initialize the Todos list
+		init : function(){
+			// Render the Todos
+			this.element.append(can.view('//todo/views/todo.ejs', {
+				todos: this.options.todos
+			}));
+
+			// Clear the new todo field
+			$('#new-todo').val('').focus();
+		},
+
+		// Listen for when a new Todo has been entered
+		'#new-todo keyup' : function(el, ev){
+			if(ev.keyCode == 13){
+				new Todo({
+					text : el.val(),
+					complete : false
+				}).save(function() {
+					el.val('');
+				});
 			}
-			success && success(instances)
-		})
-	},
-	/**
-	 * Destroys a single todo by id
-	 *     
-	 *     Todo.destroy(1, success())
-	 */
-	destroy: function(id, success){
-		this.List.destroy([id], success)
-	},
-	/**
-	 * Creates a todo with the provided attrs.  This allows:
-	 * 
-	 *     new Todo({text: 'hello'}).save( success(todo) );
-	 */
-	create: function(attrs, success){
-		this.localStore(function(todos){
-			attrs.id = attrs.id || parseInt(100000 *Math.random())
-			todos[attrs.id] = attrs;
-		});
-		success({id : attrs.id})
-	},
-	/**
-	 * Updates a todo by id with the provided attrs.  This allows:
-	 * 
-	 *     todo.update({text: 'world'}, success(todos) )
-	 */
-	update: function(id, attrs, success){
-		this.localStore(function(todos){
-			var todo = todos[id];
-			$.extend(todo, attrs);
-		});
-		success({});
-	}
-	
-},{});
+		},
 
-/**
- * Helper methods on collections of todos.  But lists can also use their model's 
- * methods.  Ex:
- * 
- *   var todos = [new Todo({id: 5}) , new Todo({id: 6})],
- *       list = new Todo.List(todos);
- *       
- *   list.destroyAll() -> calls Todo.destroyAll with [5,6].
- */
-$.Model.List('Todo.List',
-{
-	/**
-	 * Destroys a list of todos by id from localStorage
-	 *     
-	 *     Todo.destroy([1,2], success())
-	 */
-	destroy : function(ids, success, error){
-		this.namespace.localStore(function(todos){
-			$.each(ids, function(){
-				delete todos[this]
+		// Handle a newly created Todo
+		'{Todo} created' : function(list, ev, item){
+			this.options.todos.push(item);
+		},
+
+		// Listen for editing a Todo
+		'.todo dblclick' : function(el, ev) {
+			el.data('todo').attr('editing', true).save(function() {
+				el.children('.edit').focus().select();
 			});
-		});
-		success();
-	}	
-},
-{
-	
-	/**
-	 * Return a new Todo.List of only complete items
-	 */
-	completed : function(){
-		return this.grep(function(item){
-			return item.complete === true;
-		})
-	}
-});
+		},
 
+		// Update a todo
+		updateTodo: function(el) {
+			el.closest('.todo').data('todo')
+				.attr({
+					editing: false,
+					text: el.val()
+				}).save();
+		},
 
-/**
- * A Todos widget created like
- * 
- *    $("#todos").todos({ list: new Todo.List() });
- *    
- * It listens on changes to the list and items in the list with the following actions:
- * 
- *   - "{list} add"    - todos being added to the list
- *   - "{list} remove" - todos being removed from the list
- *   - "{list} update" - todos being updated in the list
- *   
- */
-$.Controller('Todos',{
-	
-	// sets up the widget
-	init : function(){
-		
-		// empties the create input element
-		this.find(".create").val("")[0].focus();
-		
-		// fills this list of items (creates add events on the list)
-		this.options.list.findAll();
-	},
-	
-	// adds existing and created to the list
-	"{list} add" : function(list, ev, items){
-	 	
-		// uses the todosEJS template (in todo.html) to render a list of items
-		// then adds those items to #list
-		this.find('#list').append("todosEJS",items)
-	 	
-		// calls a helper to update the stats info
-		this.updateStats();
-	},
-	
-	// Creating a todo --------------
-	
-	// listens for key events and creates a new todo
-	".create keyup" : function(el, ev){
-		
-		if(ev.keyCode == 13){
-			new Todo({
-				text : el.val(),
-				complete : false
-			}).save(this.callback('created'));
-			
-			el.val("");
+		// Listen for an edited Todo
+		'.todo .edit keyup' : function(el, ev){
+			if(ev.keyCode == 13){
+				this.updateTodo(el);
+			}
+		},
+		'.todo .edit focusout' : function(el, ev) {
+			this.updateTodo(el);
+		},
+
+		// Listen for the toggled completion of a Todo
+		'.todo .toggle click' : function(el, ev) {
+			el.closest('.todo').data('todo')
+				.attr('complete', el.is(':checked'))
+				.save();
+		},
+
+		// Listen for a removed Todo
+		'.todo .destroy click' : function(el){
+			el.closest('.todo').data('todo').destroy();
+		},
+
+		// Listen for toggle all completed Todos
+		'#toggle-all click' : function(el, ev) {
+			var toggle = el.prop('checked');
+			can.each(this.options.todos, function(i, todo) {
+				todo.attr('complete', toggle).save();
+			});
+		},
+
+		// Listen for removing all completed Todos
+		'#clear-completed click' : function() {
+			for (var i = this.options.todos.length - 1, todo; i > -1 && (todo = this.options.todos[i]); i--) {
+				todo.attr('complete') && todo.destroy();
+			}
 		}
-	},
-	
-	// When a todo is created, add it to this list
-	"created" : function(todo){
-		this.options.list.push(todo); //triggers 'add' on the list
-	},
-	
-	// Destroying a todo --------------
-	
-	// the clear button is clicked
-	".todo-clear click" : function(){
-		// gets completed todos in the list, destroys them
-		this.options.list.completed()
-			.destroy(); 
 
-	},
-	
-	// When a todo's destroy button is clicked.
-	".todo .destroy click" : function(el){
-		el.closest('.todo').model().destroy();
-	},
-	
-	// when an item is removed from the list ...
-	"{list} remove" : function(list, ev, items){
-		
-		// get the elements in the list and remove them
-		items.elements(this.element).slideUp(function(){
-			$(this).remove();
+	})
+
+	// Initialize the app
+	Todo.findAll({}, function(todos) {
+		new Todos('#todoapp', {
+			todos: todos
 		});
-		
-		this.updateStats();
-	},
-	
-	
-	// Updating a todo --------------
-	
-	// when the checkbox changes, update the model
-	".todo [name=complete] change" : function(el, ev){
-		var todo = el.closest('.todo').model().update({
-			complete : el.is(':checked')
-		});
-	},
-	
-	// switch to edit mode
-	".todo dblclick" : function(el){
-		var input = $("<input name='text' class='text'/>").val(el.model().text)
-		el.html(input);
-		input[0].focus();
-	},
-	
-	// update the todo's text on blur
-	".todo [name=text] focusout" : function(el, ev){
-		
-		var todo = el.closest('.todo').model().update({
-			text : el.val()
-		});
-	},
-	
-	// when an item is updated
-	"{list} updated" : function(list, ev, item){
-		item.elements().html("todoEJS", item);
-		this.updateStats();
-		//update completed
-	},
-	
-	// a helper that updates the stats
-	updateStats : function(){
-		var list = this.options.list,
-			completed = list.completed().length;
-		$("#todo-stats").html("statsEJS",{
-			completed : completed,
-			total : list.length,
-			remaining : list.length - completed
-		})
-	}
-})
-
-
-
-$(function(){
-	
-	// create a todos widget with a list
-	$("#todos").todos({list : new Todo.List()});
-})
-
+	});
 
 });
